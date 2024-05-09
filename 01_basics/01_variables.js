c1a85b733560c709e53436fdd80c0a1c0506820f
bhatshishir4 <87171276+bhatshishir4@users.noreply.github.com>
2024-05-09 06:36:53 +0000

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,7 @@ const accountId = 144553
 let accountEmail = "googaga@google.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
+let accountState;
 
 
 accountEmail = "hc@hc.com"
@@ -9,5 +10,12 @@ accountPassword = "212121"
 accountCity = "Cairo"
 
 console.log(accountId);
+
+/* 
+
+never use var always use let because of issue in block scope and functional scope
+
+*/
+
 
 console.table([accountEmail, accountId, accountPassword, accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; //use newer version of JS code
+
+// number => 2 ^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+//object 
+
+console.log(typeof null) ; // object
+console.log(typeof undefined) ; //undefined
