704b8c7bf5802462d80194e8dcd1ea4b53dc3333
bhatshishir4 <87171276+bhatshishir4@users.noreply.github.com>
2024-05-09 11:02:01 +0000

    summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -3,10 +3,22 @@
 //7types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
 
-JS is dynamic 
-
+//JS is dynamic
 
 
 //Reference Type (Non-primitive Type)
 
 //Array, Objects, Functions
+
+const heroes = ["superman", "batman", "spiderman"]
+let obj1 = {
+
+    name = Shishir
+    age = 22
+
+}
+
+const function1 = function()
+{
+    console.log("Hello world")
+}
