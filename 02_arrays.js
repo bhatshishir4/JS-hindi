b0904670a8294f3b4edc4d75e06cee0a12fdbcfb
bhatshishir4 <87171276+bhatshishir4@users.noreply.github.com>
2024-05-10 07:02:10 +0000

    arrays2

---
+++ 02_arrays.js
@@ -0,0 +1,5 @@
+const MarvelHeroes = ["Ironman","Thor","Hawkeye"]
+const DcHeroes = ["Batman","Superman","Flash"]
+
+const join = MarvelHeroes.concat(DcHeroes)
+console.log(join)
--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,5 +1,10 @@
 const myArr = [0,1,2,3,4]
 console.log(myArr)
-myArr.shift()
-console.log(myArr)
+// myArr.shift()
+
+const slice1 = myArr.slice(1,3)
+console.log(slice1)
+
+const splice1 = myArr.splice(1,3)
+console.log(splice1)
 
