c1faeac2383b667eaf01fc099ed369c423b789ab
bhatshishir4 <87171276+bhatshishir4@users.noreply.github.com>
2024-05-10 03:54:32 +0000

    min max

--- 01_basics/05_nums_and_math.js
+++ 01_basics/05_nums_and_math.js
@@ -1,13 +1,24 @@
 const score = 400.44678
 
-console.log(score.toString().length)
-console.log(score.toFixed(2))
-console.log(score.toPrecision(3))
+// console.log(score.toString().length)
+// console.log(score.toFixed(2))
+// console.log(score.toPrecision(3))
 
-const othernumber = 4012300034334
-console.log(othernumber.toLocaleString('en-IN'));//for indian leave empty for US standard
+// const othernumber = 4012300034334
+// console.log(othernumber.toLocaleString('en-IN'));//for indian leave empty for US standard
 
 
 //------------------MATHS--------------------------------------//
 
+
+// console.log(Math.pow(4,3))
+// console.log(4**3)
+
+
+// console.log(Math.ceil((Math.random()*10)+1))
+
+// Min = 10
+// Max = 20
+
+// console.log(Math.floor(Math.random() * (Max-Min+1) + Min))
 
