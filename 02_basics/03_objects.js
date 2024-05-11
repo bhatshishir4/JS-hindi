b86e50464fc87afff5d0e439d35ce76519813424
bhatshishir4 <87171276+bhatshishir4@users.noreply.github.com>
2024-05-11 11:09:46 +0000

    obj1

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,16 @@
+//singleton
+//Object.create  can also be used to create objects
+
+//object literals
+
+const JsUser = { 
+    name: "Shishir",
+    age: 23,
+    location: "Bhaktapur",
+    email: "bhatshishir4@gmail.com",
+    isLoggedin: true,
+    LastLoginDays: ["Sunday", "Monday"]
+
+}
+
+console.log(JsUser)
