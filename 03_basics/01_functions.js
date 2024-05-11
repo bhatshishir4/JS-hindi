217d1622dba4310214a737699708f414e28d1447
bhatshishir4 <87171276+bhatshishir4@users.noreply.github.com>
2024-05-11 12:16:49 +0000

    fucn

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,11 @@
+function SayMyName(){
+    console.log("S")
+    console.log("H")
+    console.log("I")
+    console.log("S")
+    console.log("H")
+    console.log("I")
+    console.log("R")
+}
+
+SayMyName()
