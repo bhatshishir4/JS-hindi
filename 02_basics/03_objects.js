747a1343966ddb760f63f6a22ea94f2dbbdd52a1
bhatshishir4 <87171276+bhatshishir4@users.noreply.github.com>
2024-05-11 11:33:49 +0000

    obj1

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -3,8 +3,11 @@
 
 //object literals
 
+const mysym1 = Symbol("key1")
+
 const JsUser = { 
     name: "Shishir",
+    [mysym1]: "Mykey",
     age: 23,
     location: "Bhaktapur",
     email: "bhatshishir4@gmail.com",
@@ -13,4 +16,29 @@ const JsUser = {
 
 }
 
-console.log(JsUser)
+
+
+// to overwrite just use this  JsUser.email = "bhatasdfadsg@gmail.com"
+//Object.freeze(JsUser)   This is will lock the object
+
+//JsUser.email = "hotpot.com"
+
+//console.log(JsUser)
+
+//console.log(JsUser[mysym1])
+//console.log(typeof JsUser[mysym1])
+
+
+JsUser.greeting = function(){
+
+console.log("Hello World")
+}
+
+JsUser.greetingtwo = function(){
+
+    console.log(`Hello World, My name is ${this.name}`)
+    }
+
+console.log(JsUser.greeting())
+console.log(JsUser.greetingtwo())
+
