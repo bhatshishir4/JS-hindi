e3b1a52011079ae16989d8226363f018a74a381b
bhatshishir4 <87171276+bhatshishir4@users.noreply.github.com>
2024-05-11 12:55:49 +0000

    ar

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,11 +1,26 @@
-function SayMyName(){
-    console.log("S")
-    console.log("H")
-    console.log("I")
-    console.log("S")
-    console.log("H")
-    console.log("I")
-    console.log("R")
+// function SayMyName(){
+//     console.log("S")
+//     console.log("H")
+//     console.log("I")
+//     console.log("S")
+//     console.log("H")
+//     console.log("I")
+//     console.log("R")
+// }
+
+//SayMyName()
+
+function CalculateCartPrice(...num1){
+    return num1
+}
+console.log(CalculateCartPrice(200,400,600))
+//... is spread operator returns array. it is also rest operator
+
+const myNewArray = [200,400,800]
+
+function SecondVal(getArray){
+
+    return getArray[1]
 }
 
-SayMyName()
+console.log("The second value is:", getArray )
