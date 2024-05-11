59ee5f944ea906f74f45d55aa8b4345eee2209f4
bhatshishir4 <87171276+bhatshishir4@users.noreply.github.com>
2024-05-11 13:04:30 +0000

    func

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -10,17 +10,30 @@
 
 //SayMyName()
 
-function CalculateCartPrice(...num1){
-    return num1
-}
-console.log(CalculateCartPrice(200,400,600))
+// function CalculateCartPrice(...num1){
+//     return num1
+// }
+//console.log(CalculateCartPrice(200,400,600))
 //... is spread operator returns array. it is also rest operator
 
-const myNewArray = [200,400,800]
+// const myNewArray = [200,400,800]
 
-function SecondVal(getArray){
+// function SecondVal(getArray){
 
-    return getArray[1]
+//     return getArray[1]
+// }
+
+// console.log("The second value is:", SecondVal(myNewArray) )
+
+
+
+
+const Name = ["Shishir","Sagun","Bibek","Suyog"]
+
+function getName(fourthname){
+    return fourthname[3]
 }
+console.log("The fourth name from the Array is:", getName(Name))
 
-console.log("The second value is:", getArray )
+
+
