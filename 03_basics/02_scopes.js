1bc8635ebdfc1f7edefa5318ab85f61f02aaa20b
bhatshishir4 <87171276+bhatshishir4@users.noreply.github.com>
2024-05-11 13:44:20 +0000

    sc

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,7 +1,24 @@
- let a = 10
- const b = 20
- var c = 30
+//  let a = 10
+//  const b = 20
+//  var c = 30
 
- console.log(a);
- console.log(b);
- console.log(c);
+//  console.log(a);
+//  console.log(b);
+//  console.log(c);
+
+  
+//  function addone(num){
+
+//     return num + 1
+//  }
+
+//  console.log(addone(5))
+//  console.log(addone(6))
+
+//  const addtwo = function(hi){
+//     return hi = "Shishir" + 1
+//  }
+//  console.log(addtwo())
+
+// -------------------arrow function -------------
+
