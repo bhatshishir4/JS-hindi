01a50b42bf7073c9c644befc9e3d5fcd4e068789
bhatshishir4 <87171276+bhatshishir4@users.noreply.github.com>
2024-05-12 06:00:10 +0000

    map

---
+++ 05_iterations/three.js
@@ -0,0 +1,22 @@
+//Maps
+
+// const map1 = new Map()
+// map1.set("NP", "Nepal")
+// map1.set("JP", "Japan")
+// map1.set("KR", "Korea")
+
+// //console.log(map1)
+// for (const [key,Values] of map1) {
+//     console.log(`${key} :- ${Values}`)
+    
+// }
+
+// const logo = new Map()
+// logo.set("AAPL", "Apple")
+// logo.set("GOOGL", "Alphabet")
+// logo.set("MSTR", "Microsoft")
+
+// for (const [key, value] of logo) {
+//     console.log(value, key)
+    
+// }
