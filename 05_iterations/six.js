8e3f315fb5aead6c457e9b6d897d1213ffd10cbc
bhatshishir4 <87171276+bhatshishir4@users.noreply.github.com>
2024-05-12 10:05:39 +0000

    six

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -1,6 +1,12 @@
 const myNums = [1,2,3,4,5,6,7,8,9,10]
 
-myNums.filter( (great) => {
-    myNums >= 5 
-    console.log(myNums)
-} )
+// const newgreatnum = myNums.filter( (great) => {
+    
+//     return great >= 5   })
+// console.log(newgreatnum)
+
+//  myNums.forEach(great => { 
+//     return great >= 5
+    
+// });
+
