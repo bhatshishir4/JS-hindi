46b6498a8da494cb621c1fa66c91055b16303fda
bhatshishir4 <87171276+bhatshishir4@users.noreply.github.com>
2024-05-12 09:34:36 +0000

    for

---
+++ 05_iterations/five.js
@@ -0,0 +1,12 @@
+const coding = ["js","ruby","java","python"]
+
+coding.forEach( function (languages) {
+
+    
+} )
+
+//coding.forEach( () => {}) )
+
+coding.forEach( (codes) => {
+    console.log(coding)
+}) 
