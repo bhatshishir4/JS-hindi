989c394443df5fc6c013ab025a0f2bcb2c35bafa
bhatshishir4 <87171276+bhatshishir4@users.noreply.github.com>
2024-05-12 04:21:53 +0000

    if

--- 04_control/one.js
+++ 04_control/one.js
@@ -1,7 +1,21 @@
 const IsLoggedIn = true
 const HasDebitCard = true
+const LoggedInFromGoogle = true
+const LoggedInFromEmail = false
+// if(IsLoggedIn && HasDebitCard)  
+// {   
+//     console.log("User can shop")
 
-if(IsLoggedIn && HasDebitCard){
-    console.log("User can shop")
+// }
 
-}
+//&& can be used many times to check 2 or more conditions. it only executes code if both is true.
+
+// if(LoggedInFromGoogle || LoggedInFromEmail){
+
+//     console.log("User has loggedin.")
+
+// }
+// || can be used to check OR i.e. if any one condition is true, code is executed. 
+
+
+
