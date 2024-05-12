80b6344084b9260b0a8950c458e8c3c9564a6373
bhatshishir4 <87171276+bhatshishir4@users.noreply.github.com>
2024-05-12 05:43:17 +0000

    loops

---
+++ 05_iterations/one.js
@@ -0,0 +1,42 @@
+//for
+
+//table upto 10
+
+// for (let i = 0; i <=10; i++) {
+//     console.log("Table of :", i )
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(i + '*' + j  + '=' , i * j);
+        
+//     }
+    
+// }
+
+//break and continue
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i == 5) {
+//         console.log("Detected 5")
+//         break;
+        
+//     }
+//     console.log("Values are", i)
+    
+// }
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i == 5) {
+//         console.log("Detected 5")
+//         continue;
+        
+//     }
+//     console.log("Values are", i)
+    
+// }
+
+// while (condition) {
+    
+// }
+
+// do {
+    
+// } while (condition);
---
+++ 05_iterations/two.js
@@ -0,0 +1,12 @@
+//for of
+
+const greetings = "Hello World!"
+
+for (const val of greetings) {
+    if (val == " ") {
+        continue;
+        
+    }
+    console.log(val)
+    
+}
