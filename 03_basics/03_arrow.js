5a19807ee110a7e7ebe214bcaa99b83952b50b7b
bhatshishir4 <87171276+bhatshishir4@users.noreply.github.com>
2024-05-12 04:07:23 +0000

    arrow func

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -1,7 +1,15 @@
 // () => {}  arrow functioon syntax
 
-const addtwo = (num1, num2) => {
-    return num1 + num2
-}
-console.log(addtwo(5, 6))
+// const addtwo = (num1, num2) => {
+//     return num1 + num2
+// }
+// console.log(addtwo(5, 6))
+
+// const addtwo = (num1, num2) => (num1 + num2)
+// console.log(addtwo(5,6))
+
+// you can use arrow function like (8-9) but you must use paranthese and not curly braces
+
+ 
+
 
