775ac6494dc5d418e6a279697b670ba9a911241c
bhatshishir4 <87171276+bhatshishir4@users.noreply.github.com>
2024-05-12 04:30:36 +0000

    switch case

---
+++ 04_control/switch.js
@@ -0,0 +1,32 @@
+
+//  const month = "September"
+
+//  switch (month) {
+//     case "Jan":
+//         console.log("Janurary");
+//         break;
+ 
+//     case "Feb":
+//         console.log("Feburary");
+//         break;
+ 
+//     case "Mar":
+//         console.log("March");
+//         break;
+ 
+//     case "April":
+//         console.log("April");
+//         break;
+ 
+//     default:console.log("Hawa aane dein")
+//         break;
+//  }
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
