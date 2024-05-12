9a17eb06e65e4ca86a9634b9c67195a62a1af61c
bhatshishir4 <87171276+bhatshishir4@users.noreply.github.com>
2024-05-12 10:29:14 +0000

    reduce

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -1,3 +1,34 @@
 //reduce
 
 
+// const mynums = [1,2,3]
+
+// const total = mynums.reduce( (acc, curr) => (acc + curr),0 )
+// console.log(total)
+
+const shoppingCart = [
+    {
+        name:"JS course",
+        price: 2999
+        
+    },
+    {
+        name:"python",
+        price: 999
+
+    },
+    {
+        name:" mobile dev",
+        price: 5999
+
+    },
+    {
+        name:"Data Science",
+        price: 12999
+
+    }
+
+]
+
+const totalprice = shoppingCart.reduce( (acc, item) => (acc + item.price ),0)
+console.log(totalprice)
